17326d9504f4c1bc85b203e121118f663c83fa72
Morax19 <danielhdez19@gmail.com>
2025-10-08 17:35:21 -0400

    Changes to be committed: 	modified:   frontend/src/components/base/Welcome.jsx

--- frontend/src/components/base/Welcome.jsx
+++ frontend/src/components/base/Welcome.jsx
@@ -59,6 +59,30 @@ function WelcomePage() {
     </div>
   );
   */
+  return (
+    <div className="containerHome">
+      {/* Curva inferior */}
+      <svg
+        className="wave-footer"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 500 150"
+        preserveAspectRatio="none"
+      >
+        <path d="M 0 20 C 80 110 160 80 240 80 C 320 40 420 80 500 60 L 500 150 L 0 150 Z" fill="#619990" />
+        <path d="M 0 60 C 150 160 330 -30 500 60 L 500 150 L 0 150 Z" fill="#a2c0bb" />
+      </svg>
+      <img src={logoHome} alt="Logo" className="logoHome" />
+      <h2>Bienvenido a Control de Garantías Gipsy</h2>
+      <input className="inputHome" type="submit" value="Ingresar" onClick={() => navigate('/login')}></input>
+      <br /><br />
+
+      <div className="welcome-footer">
+        <Link to="/technical-service" className="service-link"> 
+          Servicio Técnico
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
