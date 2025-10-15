84ba1d5b2b869b88a7a77a24c039f148fdfbe7e3
Morax19 <danielhdez19@gmail.com>
2025-10-14 23:39:42 -0400

    On branch main  Changes to be committed: 	modified:   frontend/src/assets/IMG/Gipsy_imagotipo_color.png 	new file:   frontend/src/assets/IMG/Gipsy_imagotipo_color_deprecated.png 	modified:   frontend/src/assets/IMG/Gipsy_imagotipo_medioBlanco.png 	new file:   frontend/src/assets/IMG/Gipsy_imagotipo_medioBlanco_deprecated.png 	modified:   frontend/src/assets/IMG/Gipsy_isotipo_color.png 	new file:   frontend/src/assets/IMG/Gipsy_isotipo_color_deprecated.png modified:   frontend/src/components/test/TechnicalServiceLogged.jsx

--- frontend/src/components/test/TechnicalServiceLogged.jsx
+++ frontend/src/components/test/TechnicalServiceLogged.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../styles/technical_service/technicalService.css';
 import LayoutBase from '../base/LayoutBaseUser';
+import whatsappLogo from '../../assets/IMG/whatsapp.png';
 
 const sucursalesData = [
   {
@@ -8,7 +9,9 @@ const sucursalesData = [
     nombreEmpresa: 'Call Center - Garantías',
     telefono: '+58 422-6247117',
     diaAtencion: 'Lunes a Viernes',
-    horarioAtencion: '9:00 AM - 5:00 PM'
+    horarioAtencion: '9:00 AM - 5:00 PM',
+    correo: 'info@garantiasservicio.com',
+    whatsappLink: 'https://wa.me/584226247117?text=Hola,%20quisiera%20consultar%20sobre%20una%20garantía.',
   },
 ];
 
@@ -23,9 +26,29 @@ const TechnicalService = () => {
       <div className="branch-cards-container">
         {sucursalesData.map((sucursal) => (
           <div key={sucursal.id} className="branch-card-user">
+
+            <a 
+              href={sucursal.whatsappLink} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className="whatsapp-icon-container"
+              title="Iniciar chat de WhatsApp"
+            >
+                <img src={whatsappLogo} alt="WhatsApp Logo" className="whatsapp-logo" />
+            </a>
+
             <div className="branch-card-info">
-              <div className="branch-card-title">{sucursal.nombreEmpresa}</div>
+              <div className="branch-card-title">{sucursal.nombreEmpresa}</div>           
               <div>Teléfono: {sucursal.telefono}</div>
+              <div className="contact-email">
+                Correo electrónico: 
+                <a 
+                  href={`mailto:${sucursal.correo}?subject=Consulta%20de%20Servicio%20de%20Garantías`}
+                  className="email-link"
+                >
+                  {sucursal.correo}
+                </a>
+              </div>
             </div>
             <div className="branch-card-schedule">
               <div className="branch-card-title">Horario de Atención</div>
